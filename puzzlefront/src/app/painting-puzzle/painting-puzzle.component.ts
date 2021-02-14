8cf2f7ee697483f9673e532c785518c52358a2f8
Sam Jones <sam.jones240699@gmail.com>
2021-02-14 04:03:52 +0000

    painting puzzle part 1

--- puzzlefront/src/app/painting-puzzle/painting-puzzle.component.ts
+++ puzzlefront/src/app/painting-puzzle/painting-puzzle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { BasePuzzleComponent } from '../base-puzzle/base-puzzle.component';
 
 @Component({
@@ -7,8 +7,47 @@ import { BasePuzzleComponent } from '../base-puzzle/base-puzzle.component';
   styleUrls: ['./painting-puzzle.component.scss']
 })
 export class PaintingPuzzleComponent extends BasePuzzleComponent {
-
-  ngOnInit(): void {
+  canvas : boolean[][] = [[false, false, false],
+                          [false, false, false],
+                          [false, false, false],
+                          [false, false, false],
+                          [false, false, false],
+                          [false, false, false]]
+  solution : boolean[][] = [[true, false, false],
+                            [false, false, false],
+                            [false, false, false],
+                            [false, false, false],
+                            [false, false, false],
+                            [false, false, false]]
+  filled : boolean[] = [false, false, false, false, false, false]
+  brush : boolean[] = [false, false, false];
+  assetDir = "../../assets/Painting-Jigsaw Puzzle/"
+  updateBrush(pot : number) : void {
+    this.brush[pot] = true;
+    console.log(this.brush)
   }
-
+  clearBrush() : void {
+    this.brush = [false, false, false]
+  }
+  paint(section : number) : void {
+    this.canvas[section] = [...this.brush];
+    this.filled[section] = this.brush.reduce((acc, next) => acc || next);
+  }
+  checkCanvas() : void{
+    var done = true;
+    for(var i=0;i<this.canvas.length;i++){
+      for(var j=0;j<this.canvas[i].length;j++){
+        if(this.canvas[i][j] != this.solution[i][j]){
+          done = false;
+          break;
+        }
+      }
+    }
+    console.log(this.canvas);
+    if(done){
+      this.checkAnswer("done")
+    }else{
+      this.checkAnswer("bad")
+    }
+  }
 }
